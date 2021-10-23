c78c4613256519d062b83edb221b6dd8012569b2
krisnaxyoga <yogakris.yk@gmail.com>
2021-10-23 10:28:53 +0800

    perbaikan

---
+++ src/views/Tables/Katalog/ProdukTable.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="card shadow" :class="type === 'dark' ? 'bg-default' : ''">
+    <div
+      class="card-header border-0"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0" :class="type === 'dark' ? 'text-white' : ''">
+            {{ title }}
+          </h3>
+        </div>
+        <div class="col text-right">
+          <base-button type="primary" size="sm">See all</base-button>
+        </div>
+      </div>
+    </div>
+
+    <div class="table-responsive">
+      <base-table
+        class="table align-items-center table-flush"
+        :class="type === 'dark' ? 'table-dark' : ''"
+        :thead-classes="type === 'dark' ? 'thead-dark' : 'thead-light'"
+        tbody-classes="list"
+        :data="tableData"
+      >
+        <template v-slot:columns>
+          <th>Project</th>
+          <th>Budget</th>
+          <th>Status</th>
+          <th>Users</th>
+          <th>Completion</th>
+          <th></th>
+        </template>
+
+        <template v-slot:default="row">
+          <th scope="row">
+            <div class="media align-items-center">
+              <a href="#" class="avatar rounded-circle mr-3">
+                <img alt="Image placeholder" :src="row.item.img" />
+              </a>
+              <div class="media-body">
+                <span class="name mb-0 text-sm">{{ row.item.title }}</span>
+              </div>
+            </div>
+          </th>
+          <td class="budget">
+            {{ row.item.budget }}
+          </td>
+          
+        </template>
+      </base-table>
+    </div>
+
+    <div
+      class="card-footer d-flex justify-content-end"
+      :class="type === 'dark' ? 'bg-transparent' : ''"
+    >
+      <base-pagination total="30"></base-pagination>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "produk-table",
+  props: {
+    type: {
+      type: String,
+    },
+    title: String,
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          img: "img/theme/bootstrap.jpg",
+          title: "Argon Design System",
+          budget: "$2500 USD",
+          status: "pending",
+          statusType: "warning",
+          completion: 60,
+        },
+        {
+          img: "img/theme/angular.jpg",
+          title: "Angular Now UI Kit PRO",
+          budget: "$1800 USD",
+          status: "completed",
+          statusType: "success",
+          completion: 100,
+        },
+        {
+          img: "img/theme/sketch.jpg",
+          title: "Black Dashboard",
+          budget: "$3150 USD",
+          status: "delayed",
+          statusType: "danger",
+          completion: 72,
+        },
+        {
+          img: "img/theme/react.jpg",
+          title: "React Material Dashboard",
+          budget: "$4400 USD",
+          status: "on schedule",
+          statusType: "info",
+          completion: 90,
+        },
+        {
+          img: "img/theme/vue.jpg",
+          title: "Vue Paper UI Kit PRO",
+          budget: "$2200 USD",
+          status: "completed",
+          statusType: "success",
+          completion: 100,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style></style>
